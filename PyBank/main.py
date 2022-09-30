0e0e80831e6768426bfa4532b8f5f989f327abbe
Meeyoung Park <parkmee@gmail.com>
2022-09-30 01:21:23 -0400

    completed logic and verified correct output

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,72 @@
-# In this challenge, you are tasked with creating a Python script to analyze the financial records of your company. You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: "Date" and "Profit/Losses". (Thankfully, your company has rather lax standards for accounting, so the records are simple.)
+# dependencies
+import os
+import csv
 
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
+# summarize financial data from budget_data.csv
+# set path to file
+budget_csv = os.path.join('Resources','budget_data.csv')
 
-# The total number of months included in the dataset
+# open file using read mode and specify variable to store object
+with open(budget_csv, newline='') as f:
+  
+  # initialize reader
+  reader = csv.reader(f, delimiter=',')
+  
+  # read in headers and exclude from results
+  header = next(reader)
+  # print(header) # ['Date', 'Profit/Losses']
 
-# The net total amount of "Profit/Losses" over the entire period
+  # save data to list
+  budget = list(reader)
+  
+# calculate months
+months = len(budget)
+  
+# print total number of months
+print('Financial Analysis')
+print('-------------------------------------')
+print(f'Total Months: {months}')
+  
+# initialize variables for calculations
+balance = 0
+change_amounts = []
+month_max_increase = ''
+month_max_decrease = ''
+max_increase = 0
+max_decrease = 0
 
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+# iterate through budget data to calculate the change from month-to-month and maximum increases and decreases of that change
+for i in range(len(budget)):
 
-# The greatest increase in profits (date and amount) over the entire period
+  # for loop to skip first row    
+  if i > 0:
+    
+    # save previous and current profit/loss values and month
+    previous = int(budget[i-1][1])
+    current = int(budget[i][1])
+    month = budget[i][0]
+    
+    # calculate change between current and previous profit/loss values and add to the change_amounts list
+    change = current - previous
+    change_amounts.append(change)
+    
+    if change > max_increase:
+      month_max_increase = month
+      max_increase = change
+    elif change < max_decrease:
+      month_max_decrease = month
+      max_decrease = change 
+    
+  # add profit/loss value to total budget after converting to integer
+  balance += int(budget[i][1])
 
-# The greatest decrease in profits (date and amount) over the entire period
+# print sum of the profit/losses column
+print(f'Total: ${balance}')
 
-# Your analysis should look similar to the following:
+# calculate the average change from month-to-month
+avg_change = round(sum(change_amounts)/len(change_amounts),2)
 
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $22564198
-# Average Change: $-8311.11
-# Greatest Increase in Profits: Aug-16 ($1862002)
-# Greatest Decrease in Profits: Feb-14 ($-1825558)
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+# print the average change
+print(f'Average Change: ${avg_change}')
+print(f'Greatest Increase in Profits: {month_max_increase} (${max_increase})')
+print(f'Greatest Decrease in Profits: {month_max_decrease} (${max_decrease})')
